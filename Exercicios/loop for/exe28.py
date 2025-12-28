f62432db7ecfbe15efd9b74cd45556ef48d09870
Ruann-Pablo <ruannprime@gmial.com>
2025-12-28 18:26:16 -0300

    Refactor age calculation and improve output formatting in demographic data exercise

--- Exercicios/loop for/exe28.py
+++ Exercicios/loop for/exe28.py
@@ -3,6 +3,7 @@ mais_velho = ''
 soma_idade = 0
 guarda_idade = 0
 media_idade = 0
+divisor_media = 0
 
 for i in range(1, 5):
     nome = str(input('Nome: ')).strip().title()
@@ -23,11 +24,16 @@ for i in range(1, 5):
     if sexo == 'M' and guarda_idade < idade:
         mais_velho = nome
         guarda_idade = idade
-
+    
+    divisor_media += 1
     soma_idade += idade
 
 media_idade = soma_idade / i
 
 print(f'A média de idade do grupo é de {media_idade} anos.')
-print(f'O homem mais velho tem {guarda_idade} anos se chama {mais_velho}')
+if mais_velho:
+    print(f'O homem mais velho tem {guarda_idade} anos se chama {mais_velho}')
+else:
+    print('Não há homens no grupo')
+
 print(f'Ao todo são {cont_mulher} mulheres com menos de 20 anos')
