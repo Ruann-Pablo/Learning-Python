d6f39245b57eafef08d4342df41c67f6da803158
Ruann-Pablo <ruannprime@gmial.com>
2025-12-29 00:00:12 -0300

    Add number guessing game implementation

---
+++ Exercicios/loop while/exe_while02.py
@@ -0,0 +1,24 @@
+import random
+
+num_aleatorio = random.randint(1, 50)
+guarda_resposta = 0
+cont_tentativas = 0
+
+print('Pensei em um número aleatório entre 1 e 10.')
+print('Será que você consegue adivinhar?\n')
+
+while num_aleatorio != guarda_resposta:
+    guarda_resposta = int(input('Qual o seu palpite? Responda: '))
+
+    cont_tentativas += 1
+
+    if guarda_resposta > num_aleatorio:
+        print('Menos... Tente novamente.\n')
+        continue
+
+    if guarda_resposta < num_aleatorio:
+        print('Mais... Tente novamente.\n')
+        continue
+
+print(f'Parabéns! Você acertou com {cont_tentativas} tentativas!')
+    
