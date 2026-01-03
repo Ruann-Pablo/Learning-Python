0c071b61d8f4d916f4354fc3c0bba7d657cafefa
Ruann-Pablo <ruannprime@gmial.com>
2026-01-03 20:50:20 -0300

    Refactor output formatting in arithmetic progression and add while loop implementation for progression display

--- Exercicios/loop for/exe_for04.py
+++ Exercicios/loop for/exe_for04.py
@@ -2,7 +2,7 @@ termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 decimo = termo + (10 - 1) * razao
 
-for i in range (termo, decimo + razao, razao):
-    print(i, end='-> ')
+for i in range(termo, decimo + razao, razao):
+    print(i, end=' -> ')
 
 print('PA')
---
+++ Exercicios/loop while/exe_while05.py
@@ -0,0 +1,10 @@
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+i = 1
+
+while i <= 10:
+    print(f'{termo} -> ', end='')
+    termo += razao
+    i += 1
+
+    
