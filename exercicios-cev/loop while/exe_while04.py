21049b2c9a5d5537aeee2984b2020734c30f811f
Ruann-Pablo <ruannprime@gmail.com>
2026-02-05 16:12:09 -0300

    Refatorar formatação de strings para consistência e legibilidade

--- exercicios-cev/loop while/exe_while04.py
+++ exercicios-cev/loop while/exe_while04.py
@@ -1,12 +1,12 @@
-num = int(input('Digite um número: '))
+num = int(input("Digite um número: "))
 cont = num
 fatorial = 1
 
-print(f'{num}! = ', end='')
+print(f"{num}! = ", end="")
 
 while cont > 0:
-    print(f'{cont}', end='')
-    print(' * ' if cont > 1 else ' = ', end='')
+    print(f"{cont}", end="")
+    print(" * " if cont > 1 else " = ", end="")
     fatorial *= cont
     cont -= 1
 print(fatorial)
